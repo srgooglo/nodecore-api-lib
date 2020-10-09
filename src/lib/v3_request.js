66afef7ac5da89fd46f03e5ef9933d64a61676a8
srgooglo <chippotato39@gmail.com>
2020-10-09 19:38:13 +0200

    use cache driver for axios

--- src/lib/v3_request.js
+++ src/lib/v3_request.js
@@ -1,12 +1,16 @@
 'use strict'
 import axios from "axios"
 import uri_compile from './uri_compile'
+import { setupCache } from 'axios-cache-adapter'
+import localforage from 'localforage'
+import memoryDriver from 'localforage-memoryStorageDriver'
 
 export default async (payload, callback) => {
   if(!payload || !payload.endpoint || !payload.endpointList) return false
   let { prefix, endpointList, endpoint, access_token, server_key, body, options } = payload
 
   let { method, url } = await uri_compile(endpointList, endpoint, prefix)
+  await localforage.defineDriver(memoryDriver)
 
   let payloadContainer = new FormData()
   let requestOptions = {
@@ -26,6 +30,28 @@ export default async (payload, callback) => {
     payloadContainer.append('server_key', server_key)
   }
 
+
+  const forageStore = localforage.createInstance({
+    driver: [
+      localforage.INDEXEDDB,
+      localforage.LOCALSTORAGE,
+      memoryDriver._driver
+    ],
+    name: `${prefix}_${endpoint}`
+  })
+
+  const cache = setupCache({
+    maxAge: 15 * 60 * 1000,
+    store: forageStore
+  })
+
+  requestOptions.maxAge != null? (cache.maxAge = requestOptions.maxAge) : null
+
+  const api = axios.create({
+    adapter: cache.adapter
+  })
+
+
   if(body != null){
     try {  
       const bodyLength = Object.keys(body).length
@@ -34,7 +60,7 @@ export default async (payload, callback) => {
           const ent = Object.entries(body)[index]
           const key = ent[0]
           const value = ent[1]
-  
+
           payloadContainer.append(key, value)
         }
       }
@@ -43,8 +69,8 @@ export default async (payload, callback) => {
     }
   }
 
-  axios({url, method: requestOptions.method, timeout: requestOptions.timeout, data: payloadContainer, headers: {'Content-Type': 'multipart/form-data' }})
-    .then((response) => {
+  api({url, method: requestOptions.method, timeout: requestOptions.timeout, data: payloadContainer, headers: {'Content-Type': 'multipart/form-data' }})
+    .then(async (response) => {
         return callback(false, response.data)
     })
     .catch((error) => {
